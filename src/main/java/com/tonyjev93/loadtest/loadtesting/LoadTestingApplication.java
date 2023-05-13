93325e40436ebd6a1847b3c3d219161f8593ab82
TonyJev93 <tonyJev93@gmail.com>
2023-05-13 10:10:15 +0900

    init

---
+++ src/main/java/com/tonyjev93/loadtest/loadtesting/LoadTestingApplication.java
@@ -0,0 +1,13 @@
+package com.tonyjev93.loadtest.loadtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoadTestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tonyjev93/loadtest/loadtesting/LoadTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonyjev93.loadtest.loadtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
